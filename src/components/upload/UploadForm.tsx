30b8749d2050275d80dd3b188f565ad1834b6a48
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-03-28 18:58:41 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -71,7 +71,7 @@ const UploadForm = () => {
 
       const uploadResult = (await uploadPromise) as unknown as UploadResponse[];
 
-      // Dismiss loading toast and sh
+      // Dismiss loading toast and 
       toast.dismiss(uploadLoadingToast);
       toast.success("📄 PDF Uploaded Successfully!");
 
