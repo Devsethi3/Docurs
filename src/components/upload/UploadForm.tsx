21bde0ced635894d2cecb290213d5e6cf014c4f6
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-03 06:46:51 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -86,7 +86,7 @@ const UploadForm = () => {
 
       const summaryLoadingToast = toast.loading("🤖 Generating Summary...");
 
-      // Generate 
+      // Genera
       const summaryResponse = (await generatePdfSummary(
         uploadResult
       )) as unknown as SummaryResponse;
