7e5567c6245ba1ea28c89bee61d43102530ef645
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-04 20:28:01 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -92,7 +92,7 @@ const UploadForm = () => {
       )) as unknown as SummaryResponse;
       setSummaryResult(JSON.stringify(summaryResponse));
 
-      // Dismiss loading
+      // Dismiss load
       toast.dismiss(summaryLoadingToast);
 
       if (!summaryResponse.success || !summaryResponse.data) {
