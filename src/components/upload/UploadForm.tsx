6023df723775ff8269f24da358a7b5c3a407bb5d
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-09 22:00:15 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -104,7 +104,7 @@ const UploadForm = () => {
 
       toast.success("✅ Summary Generated!");
 
-      // Ex
+      //
       const summaryData = summaryResponse.data;
       let summaryText = "";
 
