26f2bd200c53a53d95c79c1a33cb9297dae2bebd
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-13 19:47:31 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -130,7 +130,7 @@ const UploadForm = () => {
 
       const savingLoadingToast = toast.loading("💾 Saving Summary...");
 
-      // Store the 
+      // Store t
       const saveResult = await storePdfSummaryAction({
         summary: summaryText,
         fileUrl: uploadResult[0].serverData.file.ufsUrl,
