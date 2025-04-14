3fafde32d6a67cceab6373680bcf7df02be18cf3
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-14 22:09:06 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -137,7 +137,7 @@ const UploadForm = () => {
         fileName: summaryData.name,
       });
 
-      // Dismiss
+      // Dismi
       toast.dismiss(savingLoadingToast);
 
       if (saveResult.success) {
