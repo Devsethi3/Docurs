22d83fb74a5475dcf9c77c516f03632d8dcd1ee7
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-15 22:05:42 +0530

    Update UploadForm.tsx

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -146,7 +146,7 @@ const UploadForm = () => {
       }
 
       formRef.current?.reset();
-      // Todo: Redirect to the [i
+      // Todo: Redirect to the 
 
     } catch (error) {
       toast.error(
