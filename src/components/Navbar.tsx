504bf017279bad27aff3cf150fb1ee186d1c4856
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-22 21:47:23 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
           </motion.div>
         )}
 
-        {/* CTA Butt */}
+        {/* CTA But */}
         <motion.div
           className="hidden md:block"
           initial={{ opacity: 0 }}
