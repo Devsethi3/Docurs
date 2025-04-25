31cdb7dc3bb46818c39c4aca5c4a42edbe27a78a
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-04-25 23:35:21 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,7 +62,7 @@ const Navbar = () => {
           )}
         </div>
 
-        {/* Mo */}
+        {/* M */}
         <div className="md:hidden">
           <button
             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
