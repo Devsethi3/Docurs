d567b79179e728b816594fa10898ea2cf74c04c0
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-05-06 23:50:47 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -143,7 +143,7 @@ const Navbar = () => {
               </Link>
             </>
           )}
-          {/* <button className="bg-primary hover:bg-primary/90 text-primary-foreground px-6 p">
+          {/* <button className="bg-primary hover:bg-primary/90 text-primary-foreground px-6">
             
           </button> */}
         </motion.div>
