dc4b80aa7054ff872cb318511804ff89f9acff3f
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-05-22 23:26:08 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -129,7 +129,7 @@ const Navbar = () => {
               <UserButton
               appearance={{
                 elements: {
-                  // userButtonAvatarBox: "w",
+                  // userButtonAvatarBox: "",
                   userButtonPopoverFooter: "hidden",
                 },
               }}
